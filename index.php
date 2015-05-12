07c22537ff1caa730810aa40ae8feaa4019dc2bc
ivan milinkovic <ivan.milinkovic@gmail.com>
2015-05-12 20:49:25 +0200

    define controller, method, parameter

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
+$url=explode('/',$_GET['url']);
+print_r($url);
+require 'controllers/'.$url[0].'.php';
+$controller=new $url[0];
+if (isset($url[2]))
+	{
+		$controller->{$url[1]}($url[2]); // $controller->function() asigns method to controller and parameter1
+	}
+else
+	{
+		if (isset($url[1]))
+			{
+				$controller->{$url[1]}(); // $controller->function() asigns method to controller
+			}
+	}
 
 ?>
-start
+
